e711569383a96d472244ffa5527728feee8d4c95
mcooper <mw.coop.r@gmail.com>
2020-08-18 13:46:07 -0400

    set graph colors; suppress messages; use hms for in-filling

--- analyze.R
+++ analyze.R
@@ -1,10 +1,10 @@
-library(tidyverse)
-library(lubridate)
-library(tcltk)
-library(scales)
-library(hms)
-library(chron)
-library(zoo)
+suppressMessages(suppressWarnings(library(tidyverse, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library(lubridate, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library(tcltk, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library(scales, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library(hms, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library(chron, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library(zoo, warn.conflicts = F, quietly = T)))
 
 today <- substr(now(), 1, 10)
 
@@ -14,14 +14,12 @@ dat <- read.delim(paste0('~/timetracker/log/', today),
 
 
 #Get rid of second bc we dont need that precision
-dat$time <- substr(dat$time, 1, 5)
+dat$time <- as_hms(paste0(substr(dat$time, 1, 5), ':00'))
 
-hours <- seq(as.numeric(substr(min(dat$time), 1, 2)),
-             as.numeric(substr(max(dat$time), 1, 2)))
-
-combs <- expand.grid(hours, substr(100:159, 2, 3))
-range <- paste0(combs$Var1, ':', combs$Var2)
-range <- range[range > min(dat$time) & range < max(dat$time)]
+range <- seq(as.numeric(min(dat$time)), 
+             as.numeric(max(dat$time)),
+             by=60) %>%
+  as_hms
 
 dat <- merge(dat,
              data.frame(time=range), 
@@ -30,32 +28,35 @@ dat <- merge(dat,
 dat$time <- as_hms(paste0(substr(dat$time, 1, 5), ":00"))
 
 workwords <- c("Slack", "@", "pdf", "Stack", "Overleaf", "Gmail", "Zoom", "Ask Ubuntu", 
-               'Editorial Manager', 'LibreOffice')
+               'Editorial Manager', 'LibreOffice', "Google Drive", "Google Slides",
+               'SpringerLnk', 'PNAS', 'PLOS', 'R ')
 
 emailwords <- c("Gmail", "Outlook")
 
 slackwords <- c("The New York Times", "Spotify", "The Atlantic", "reddit", "Twitter", 
-                "Site Blocked", 'YouTube')
+                "Site Blocked", 'YouTube', 'craigslist', 'dataisbeautiful')
 
 
 dat <- dat %>%
   mutate(task = case_when(is.na(window) ~ "Idle/Break",
+                          window == '' ~ "Idle/Break",
                           grepl(paste(workwords, collapse='|'), window) ~ "Work",
                           grepl(paste(emailwords, collapse='|'), window) ~ "Email",
-                          grepl(paste(slackwords, collapse='|'), window) ~ "Slack"),
-         task = na.locf(task, na.rm=F, maxgap=2))
+                          grepl(paste(slackwords, collapse='|'), window) ~ "Slack",
+                          TRUE ~ "Unknown"),
+         task = na.locf(task, na.rm=F, maxgap=1))
 
 dat$y <- 1
 
 
 dat <- dat[!duplicated(dat$time), ]
 
-t <- data.frame(table(dat$task, useNA='always'))
+t <- data.frame(table(dat$task))
 t$hours <- as_hms(t$Freq*60)
 
 str <- paste0(paste0(t$Var1, ': ', t$hours), collapse=' - ')
 
-X11(width=24, height=4)
+X11(width=18, height=4)
 ggplot(dat) + 
   geom_bar(aes(x=time, y=y, fill=task), stat='identity', width=60) +
   scale_x_time(expand=c(0,0), breaks=breaks_width('1 hour')) + 
@@ -65,6 +66,14 @@ ggplot(dat) +
         axis.ticks.y=element_blank(),
         plot.title = element_text(hjust = 0.5)) + 
   labs(x=today,
-       title=str)
+       title=str) + 
+  scale_fill_manual(values=c('Email'='#377eb8',
+                             'Work'='#4daf4a',
+                             'Slack'='#e41a1c',
+                             'Idle/Break'='#dddddd',
+                             'Unknown'='#333333'))
+                    
+unknowns <- dat$window[dat$task=='Unknown']
+print(unknowns)
 
 capture <- tk_messageBox(message="hit spacebar to close plots")
